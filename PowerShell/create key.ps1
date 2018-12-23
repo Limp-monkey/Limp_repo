1c633be6dae54d0a5a035d07279f907aeea0d807
limp_monkey <jan.p.kolodziejczyk@gmail.com>
2018-12-23 18:32:28 +0100

    first

---
+++ PowerShell/create key.ps1
@@ -0,0 +1,3 @@
+﻿$secureStringPwd = "<insert key here>" | ConvertTo-SecureString -AsPlainText -Force
+$secureStringText = $secureStringPwd | ConvertFrom-SecureString
+Set-Content "Path\to\file\with\password.txt" $secureStringText  
---
+++ PowerShell/testing with getting all resources from one RG.ps1
@@ -0,0 +1,8 @@
+﻿##Connect-AzAccount
+$DataLakeSub = Get-AzSubscription | Select-Object Name, Id | where Name -eq $Subscription
+
+$DataLakeRG = get-azresourcegroup |  Where {$_.ResourceGroupName -eq $ResourceGroupName}
+
+$DataLakeRG = $DataLakeRG | foreach { $_.ResourceGroupName } 
+
+Get-AzResource | where {$_.ResourceGroupName -eq $DataLakeRG} | Select-Object Name,  ResourceType, Location
